54cffe543429029b843af5160dcf49651372253a
Alexandr <l4legenda@gmail.com>
2020-09-25 22:44:37 +0500

    Даизайн Расписания
    Добавил дизайн расписания, функционала еще нет.

---
+++ app/src/main/java/ru/l4legenda/ktk45/timetable.java
@@ -0,0 +1,15 @@
+package ru.l4legenda.ktk45;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class timetable extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timetable);
+    }
+}
