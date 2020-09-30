57f638f3315682d48433545f54c7f98369191206
Alexandr <l4legenda@gmail.com>
2020-09-30 09:03:34 +0500

    fix bug is "not file is drawble"
    Исправил ошибку с импортом картинок в drawable

---
+++ app/src/main/java/ru/l4legenda/ktk45/timetable_setting.java
@@ -0,0 +1,44 @@
+package ru.l4legenda.ktk45;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public class timetable_setting extends AppCompatActivity {
+
+    String[] BranchData = {"one", "two", "three", "four", "five"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timetable_setting);
+
+        ArrayAdapter<String> BranchAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, BranchData);
+        BranchAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(BranchAdapter);
+        // заголовок
+        spinner.setPrompt("Title");
+        // выделяем элемент
+        spinner.setSelection(2);
+        // устанавливаем обработчик нажатия
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                // показываем позиция нажатого элемента
+                Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
+    }
+}
