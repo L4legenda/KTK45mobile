6d97a40f9b873341ea7f4a4a11311cb08df6d3c9
Alexandr <l4legenda@gmail.com>
2020-09-30 19:42:04 +0500

    Добавил основу для Дневника
    Основа дизайна для дневника.

---
+++ app/src/main/java/ru/l4legenda/ktk45/diary.java
@@ -0,0 +1,14 @@
+package ru.l4legenda.ktk45;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class diary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary);
+    }
+}
